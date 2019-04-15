1f0b37e643ecf785e379ea766f104809a43ae1fb
Neishol <49660729+Neishol@users.noreply.github.com>
2019-04-15 18:57:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-var serialPort = require("serialport");
+/*var serialPort = require("serialport");
 //var serial = new serialPort('COM3',{parser: serialPort.parsers.readline('\n')});
 var serial = new serialPort('COM4', {
 	parser: serialPort.parsers.readline("\n")
   });
-
-serial.on("data",function(data){
+*/
+var SerialPort = require('serialport');
+var serialPort = new SerialPort('COM4', {
+    baudRate: 9600
+});
+// Open the port
+var port = new SerialPort("/dev/ttyACM0", {
+    baudrate: 9600,
+    parser: SerialPort.parsers.readline("\n")
+});
+// Read the port data
+port.on("open", function () {
+    console.log('open');
+    port.on('data', function(data) {
+        console.log(data);
+    });
+});
+// Switches the port into "flowing mode"
+serialPort.on('data', function (data) {
+    console.log('Data:', data);
+});
+// Read data that is available but keep the stream from entering //"flowing mode"
+serialPort.on('readable', function () {
+    console.log('Data:', port.read());
+});
+/*serial.on("data",function(data){
 	
 	console.log(data.toString());
 	
 	
-});
+});*/ 
